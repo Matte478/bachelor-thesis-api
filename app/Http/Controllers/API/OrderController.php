5bc77a5f5e28616e6d0fd424b6010ac592796d12
dave <david.spak478@gmail.com>
2020-03-17 19:07:20 +0100

    store/update array of orders

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -17,61 +17,47 @@ class OrderController extends Controller
 
 
     /**
-     * @param StoreOrder $request
+     * @param StoreOrder|Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function store(StoreOrder $request)
+    public function store(Request $request)
+//    public function store(StoreOrder $request)
     {
-        $sanitized = $request->validated();
+//        TODO: array validation
+//        $sanitized = $request->validated();
 
         $user = auth()->user();
         $company = Company::find($user->company_id);
 
-        $meal = Meal::find($sanitized['meal_id']);
-        $restaurant = $meal->restaurant()->first();
+        foreach($request->data as $data)
+        {
+            if($data['meal'] == null) continue;
 
-        $agreement = Agreement::where('company_id', $company->id)
-                                ->where('restaurant_id', $restaurant->id)
-                                ->first();
+            $meal = Meal::find($data['meal']);
+            $restaurant = $meal->restaurant()->first();
 
-        if(!$agreement || !$agreement->confirmed)
-            return response()->json(['error' => 'Unauthorised'], 401);
+            $agreement = Agreement::where('company_id', $company->id)
+                ->where('restaurant_id', $restaurant->id)
+                ->first();
 
-        $sanitized['meal'] = $meal->meal;
-        $sanitized['price'] = $meal->price;
-        $sanitized['discount_price'] = $meal->price;
-        $sanitized['user_id'] = $user->id;
-        $sanitized['restaurant_id'] = $restaurant->id;
+            if(!$agreement || !$agreement->confirmed)
+                continue;
 
-        Order::create($sanitized);
+            $fullData = [
+                'meal_id' => $meal->id,
+                'meal' => $meal->meal,
+                'price' => $meal->price,
+                'discount_price' => $meal->price,
+                'user_id' => $user->id,
+                'restaurant_id' => $restaurant->id,
+                'date' => $data['date']
+            ];
 
-        return response()->json(['success' => 'success'], $this->successStatus);
-    }
-
-    public function update(UpdateOrder $request)
-    {
-        $sanitized = $request->validated();
-
-        $user = auth()->user();
-        $company = Company::find($user->company_id);
-
-        $meal = Meal::find($sanitized['meal_id']);
-        $restaurant = $meal->restaurant()->first();
-
-        $agreement = Agreement::where('company_id', $company->id)
-            ->where('restaurant_id', $restaurant->id)
-            ->first();
-
-        if(!$agreement || !$agreement->confirmed)
-            return response()->json(['error' => 'Unauthorised'], 401);
-
-        $sanitized['meal'] = $meal->meal;
-        $sanitized['price'] = $meal->price;
-        $sanitized['discount_price'] = $meal->price;
-        $sanitized['user_id'] = $user->id;
-        $sanitized['restaurant_id'] = $restaurant->id;
-
-        Order::create($sanitized);
+            Order::updateOrCreate(
+                ['user_id' => $user->id, 'date' => $data['date']],
+                $fullData
+            );
+        }
 
         return response()->json(['success' => 'success'], $this->successStatus);
     }
