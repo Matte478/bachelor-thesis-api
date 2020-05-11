b95801892456ad7139ac3ef6585aa668d910e60e
dave <david.spak478@gmail.com>
2020-05-11 18:44:44 +0200

    orders status - added permissions :lock:

--- app/Http/Requests/API/Order/StatusOrder.php
+++ app/Http/Requests/API/Order/StatusOrder.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\API\Order;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
 
 class StatusOrder extends FormRequest
 {
@@ -13,7 +14,7 @@ class StatusOrder extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return Gate::allows('order.status');
     }
 
     /**
